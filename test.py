fa574af6c1ca7efdda50893d27d361b524cdbe76
ashishkhadye33 <ashishkhadye33@gmail.com>
2025-06-11 15:20:30 +0530

    session3

---
+++ test.py
@@ -0,0 +1,7 @@
+library={
+    "Marathi":["Shrimanyogi","Chava","PuLa"],
+    "Historical":["Sambhaji","Shahu","Savarkar"],
+    "Science":["Kimayagar","Taramandal"]
+}
+
+print("Marathi Books: ", library["Marathi"])
